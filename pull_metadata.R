016e8e042ccb515ef18737f2b29cc215b0d0da6f
Stefan Nelson <stefanwnelson@gmail.com>
2021-11-15 16:49:36 -0700

    Fixed an issue with the pull where all values were contained in lists. Adjusted some datetime columns

--- pull_metadata.R
+++ pull_metadata.R
@@ -2,10 +2,12 @@
 # for more info, see https://bioconnector.github.io/workshops/r-ncbi.html#entrez_fetch()
 library(rentrez)
 library(XML)
+library(lubridate)
+library(dplyr)
 
 # May be helpful to have NCBI API key as described 
 # here: https://cran.r-project.org/web/packages/rentrez/vignettes/rentrez_tutorial.html#getting-started-with-the-rentrez
-set_entrez_key("")
+set_entrez_key("11c1de631580da44a5ae812abb5357a3ff09")
 Sys.getenv("ENTREZ_KEY")
 
 # We're looking at the assembly database; here are the options for it
@@ -19,14 +21,44 @@ res = entrez_search(db = "assembly", term = "eucaryotes",
 # The summary function takes a bunch of ids in a database, returning all the 
 # needed metadata to munge!
 step = 300
-sum = entrez_summary(db = "assembly", id = res$ids[1:(step-1)], always_return_list = TRUE)
+sum = entrez_summary(db = "assembly", id = res$ids[1:(step-1)], always_return_list = FALSE)
 for (i in seq(step,length(res$ids), step)) {
   print("i:")
   print(i)
-  new = entrez_summary(db = "assembly", id = res$ids[i:(i+step-1)], always_return_list = TRUE)
+  new = entrez_summary(db = "assembly", id = res$ids[i:(i+step-1)], always_return_list = FALSE)
   sum = append(sum, new)
   Sys.sleep(0.1)
 }
 
-save(sum, file = "metadata.RData")
+#############################################################################
+# Taking the list of list structure down to a data.frame
+#############################################################################
+metadata_df = as.data.frame(do.call("rbind", sum))
+
+# Remove column names
+rownames(metadata_df) = NULL
+
+# Remove uneeded/empty/unusable columns
+metadata_subset = metadata_df %>% select(-rsuid, -latestaccession, -synonym, 
+                                         -ucscname, -ensemblname, -assemblyclass,
+                                         -gb_bioprojects, -gb_projects, -rs_bioprojects,
+                                         -rs_projects, -biosource, -anomalouslist,
+                                         -exclfromrefseq, -propertylist, -fromtype,
+                                         -ftppath_assembly_rpt, -ftppath_stats_rpt,
+                                         -ftppath_regions_rpt, -ftppath_genbank,
+                                         -ftppath_refseq, -busco, -meta)
+
+# API pull made everything into a list; here's a quick fix
+for (name in colnames(metadata_subset)) {
+  metadata_subset[[name]] = unlist(metadata_subset[[name]]) 
+}
+
+metadata_subset$asmupdatedate = as.Date(metadata_subset$asmupdatedate)
+metadata_subset$lastupdatedate = as.Date(metadata_subset$lastupdatedate)
+metadata_subset$seqreleasedate = as.Date(metadata_subset$seqreleasedate)
+metadata_subset$submissiondate = as.Date(metadata_subset$submissiondate)
+
+
+# Export for visualizations
+save(metadata_subset, file = "metadata.RData")
 
